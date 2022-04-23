79c52c4948187fad3240d051bcfb16ff07e7bbf7
ambertide <egeemirozkan24@gmail.com>
2022-04-23 06:27:54 +0300

    Add google cloud resources

---
+++ scripts/deploy-pubsub.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE_DIR="${DIR}/../src"
+
+source "${DIR}/.env.local"
+
+gcloud functions \
+  deploy ${FUNCTION_NAME_PUBSUB} \
+  --source=${SOURCE_DIR} \
+  --runtime=python39 \
