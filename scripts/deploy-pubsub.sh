f80462eb5eb75acc192bfb9d49230d4324a45125
ambertide <egeemirozkan24@gmail.com>
2022-04-23 07:27:49 +0300

    change values

--- scripts/deploy-pubsub.sh
+++ scripts/deploy-pubsub.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SOURCE_DIR="${DIR}/../src"
 
 gcloud functions \
   deploy "tweet-news" \
-  --source="/" \
+  --source="${DIR}/.." \
   --runtime=python39 \
   --trigger-topic="tweet"
